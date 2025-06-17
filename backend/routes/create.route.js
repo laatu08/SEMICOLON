79629ec78b07c659ba671e46a07638c4877046c4
laatu08 <partha2002borah@gmail.com>
2025-06-17 18:51:59 +0530

    fix

--- backend/routes/create.route.js
+++ backend/routes/create.route.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const {createUser}=require('../controllers/user.controller.js')
 const {createCase}=require("../controllers/case.controller.js")
-const { upload,uploadFile } = require("../controllers/fileupload.controller.js")
+const { upload,uploadFile } = require("../controllers/fileUpload.controller.js")
 
 
 router=express.Router()
