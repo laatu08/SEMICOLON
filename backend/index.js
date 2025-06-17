32657aa2033062ef694e28e1781cd73b2127bbae
laatu08 <partha2002borah@gmail.com>
2025-06-17 18:55:35 +0530

    fix 2

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ dotenv.config()
 
 const app=express()
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}))
+app.use(cors())
+// app.use(cors({origin: "http://localhost:5173", credentials: true}))
 app.use(express.json())
 app.use("/uploads", express.static("uploads"));
 
